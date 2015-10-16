15db97fd50cb209cab25a4bfc44631a9d69244d6
knames <okenso@gmail.com>
2015-10-16 15:25:19 -0600

    turret is broken
    took it off the scene, just merging so we can have text file scenes and
    better version control

---
+++ Assets/Scripts/shootObjectAt.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shootObjectAt : MonoBehaviour {
+
+	public GameObject projectile;
+	int projectileLayer;
+	public Vector3 projectileOffset = new Vector3 (0f, 0f, 0);
+	public Transform target;
+
+	public float expireTime = 2.0f;
+	public float speed = 3.0f;
+	public float cooldownTimer = 0;
+	public float fireDelay = 0.25f; // maybe this could be randomized over a range of time in the future?
+
+	
+	GameObject[] orbiters;
+	
+	// Use this for initialization
+	void Start () {
+		projectileLayer = gameObject.layer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cooldownTimer -= Time.deltaTime;
+		if (cooldownTimer <= 0) {
+			Vector3 offset = transform.rotation * projectileOffset;
+			GameObject projectileGo = (GameObject)Instantiate (projectile, transform.position + offset +(target.position - transform.position).normalized, 
+			                                                   Quaternion.LookRotation(target.position - transform.position));
+			projectileGo.layer = projectileLayer;
+			Destroy (projectileGo,expireTime);
+			cooldownTimer = 1;
+		}
+
+	}
+}
