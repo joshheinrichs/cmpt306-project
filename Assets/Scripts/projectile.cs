5c682cd0ea81448d87f7c00f89f4d3d11f28e323
knames <okenso@gmail.com>
2015-10-18 21:24:02 -0600

    Implemented turret
    Turret can rotate, or create projectiles facing the player. I haven't
    yet finished t he heat seaking projectile method.

---
+++ Assets/Scripts/projectile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectile : MonoBehaviour {
+
+	public float maxSpeed = 5f;
+	public Transform heatTarget = null; // fill this in if youd like head seaking bullets
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (heatTarget == null)
+		{
+			Vector3 pos = transform.position;
+			Vector3 velocity = new Vector3(0, maxSpeed * Time.deltaTime, 0);
+			pos += transform.rotation * Quaternion.Euler(0, 0, 0) * velocity;
+			transform.position = pos;
+		}
+		else
+		{
+			// code for heatseeking, do this later
+			Debug.Log("Heat seaking not yet implemented");
+		}
+	}
+}
