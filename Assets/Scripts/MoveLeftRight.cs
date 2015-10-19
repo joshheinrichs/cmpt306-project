470e769a993a0f4c5edf47f42c920afd23d4e4b6
Makedonitsi <markustoivanen123@gmail.com>
2015-10-18 22:51:05 -0600

    Added tile movement and prefabs
    Created two scripts for moving tiles. One that goes left and right, one
    that goes up and down. Also created two tile prefabs. One red one that
    kills, one black one that just pushes the player.

---
+++ Assets/Scripts/MoveLeftRight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLeftRight : MonoBehaviour {
+
+	private float xSpeed;
+	public float moveSpeed = 2.0f;
+	float startX;
+	public float range = 5.0f;
+	
+	// Use this for initialization
+	void Start () {
+		
+		startX = transform.position.x;
+		xSpeed = -moveSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (startX - transform.position.x > range) {
+			xSpeed = moveSpeed;
+		} else if (startX - transform.position.x < -range) {
+			xSpeed = -moveSpeed;
+		}
+		transform.Translate (xSpeed * Time.deltaTime, 0, 0);
+		
+	}
+}
