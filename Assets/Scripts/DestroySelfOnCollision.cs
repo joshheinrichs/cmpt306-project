6ea9d91130763fb8f01432e56acc219d4028ceff
Josh Heinrichs <joshiheinrichs@gmail.com>
2015-10-19 13:04:34 -0600

    Add DestroySelfOnCollision behaviour

---
+++ Assets/Scripts/DestroySelfOnCollision.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroySelfOnCollision : MonoBehaviour {
+
+	public float delay = 0.0f;
+	
+	void OnCollisionEnter2D(Collision2D collision) {
+		Destroy (gameObject, delay);
+	}
+}
