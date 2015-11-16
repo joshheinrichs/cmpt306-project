a54d03236b256215458ed21fecbb676cf7b4e9f6
Gedsaro <gedsaro@hotmail.com>
2015-11-16 13:42:46 -0600

    Added 3 rooms, all obstacle based. modified the MoveUpDownScript so that the object its attachted to doesn't have to be centered, basically the same change made to the MoveLeftRightScript, except vertically based. Added a roomScripts subfolder to scripts, and added SpikeRoomStartScipt, wanted to try out a room where the spikes that appear are semi-randomized(always one specific clear path between doors, but otherwise random. likely better way to do it than this, (on creation of room randomly removes some spikes).

---
+++ Assets/Scripts/RoomScrips/SpikeRoomStartScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpikeRoomStartScript : MonoBehaviour {
+
+	public float spikeChance = 80; //%chance that spike in room is spawned(not removed) before play
+
+	// Use this for initialization
+	void Start () {
+		InitializeRoom ();//called when room created, maybe call at diff time in future.
+	}
+
+	/**
+	 * called to initialize/activate the room, for now just calling it when room created
+	 * may be called upon entering the room in the futrue?
+	 * */
+	void InitializeRoom()
+	{
+		GameObject obs = gameObject.transform.Find ("Obstacles").gameObject;
+
+		Transform[] childList = obs.GetComponentsInChildren<Transform> ();
+		for (int i =0; i < childList.Length; i++) {
+			if (childList [i].gameObject.tag == "spikes") {
+				if (Random.Range (0, 100) > spikeChance)//if ran roll > spikechance destroy spike
+					GameObject.Destroy (childList [i].gameObject);
+			}
+		}
+
+	}
+
+}
