954e7c348599c52603ce29cfd6d105db6d124e02
dam428 <dam428>
2015-11-29 20:02:29 -0600

    Added a boulder puzzle room.
    Not much of a "puzzle."  Doors open when all orange switches are covered
    and close again if one becomes uncovered.

---
+++ Assets/Scripts/RoomScrips/OpenDoorsSwitch.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Calls RoomController's Completed function once all the
+ * switches have boulders on them. This script only works 
+ * if all switches are placed within an "FloorSwitches" child.
+ */
+public class OpenDoorsSwitch : MonoBehaviour
+{
+    bool done;
+    Transform switches;
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name == "FloorSwitches")
+            {
+                switches = child;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        done = true;
+
+        foreach (Transform child in switches)
+        {
+            if (!child.GetComponent<SwitchPressed>().get())
+            {
+                done = false;
+            }
+        }
+
+        // all switches are covered
+        if (done)
+        {
+            GetComponent<RoomController>().Completed();
+        }
+        // not all covered
+        else
+        {
+            GetComponent<RoomController>().Unfinished();
+        }
+    }
+}
