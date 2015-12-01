bc8a513f2c84f92b54eae7080568fb7202f72dbf
dam428 <dam428>
2015-11-30 20:34:40 -0600

    2 new rooms, updated boulder room.
    A room with switches to shoot and another boulder pushing room.  Boulder
    rooms have reset switches now.  If the boulder positions are reset while
    some are on top of a switch, the switch still thinks there is a boulder
    on it...

---
+++ Assets/Scripts/RoomScrips/OpenDoorsWallSwitch.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Calls RoomController's Completed function once all the
+ * switches have boulders on them. This script only works 
+ * if all switches are placed within an "WallSwitches" 
+ * child.
+ */
+public class OpenDoorsWallSwitch : MonoBehaviour
+{
+    bool done;
+    Transform switches;
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name == "WallSwitches")
+            {
+                switches = child;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        done = true;
+
+        foreach (Transform child in switches)
+        {
+            if (!child.GetComponent<WallSwitch>().get())
+            {
+                done = false;
+            }
+        }
+
+        // all switches are covered
+        if (done)
+        {
+            GetComponent<RoomController>().Completed();
+        }
+        // not all covered
+        else
+        {
+            GetComponent<RoomController>().Unfinished();
+        }
+    }
+}
