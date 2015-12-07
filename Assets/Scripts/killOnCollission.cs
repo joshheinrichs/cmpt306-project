0b912c444aad604b6de148ce8b579db8b59570ed
Josh Heinrichs <joshiheinrichs@gmail.com>
2015-12-07 17:19:28 -0600

    kill on collision now does 25 damage

--- Assets/Scripts/killOnCollission.cs
+++ Assets/Scripts/killOnCollission.cs
@@ -13,7 +13,7 @@ public class killOnCollission : MonoBehaviour {
 	void OnCollisionStay2D(Collision2D collision) {
 		if (collision.gameObject.tag == "Player") {
 			PlayerHealth player = collision.gameObject.GetComponent<PlayerHealth> ();
-			player.TakeDamage(10);
+			player.TakeDamage(25);
 //			player.Death ();
 		}
 	}
