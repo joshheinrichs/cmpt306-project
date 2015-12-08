858a9ad28ddf19e8f81d0151140b195043690d30
Gedsaro <gedsaro@hotmail.com>
2015-12-07 22:51:39 -0600

    -changed order in layer of all enemies to be 1, so that they appear over top of most obstacle type things (now they go over, instead of under healthpacks for example) -made it so that healthpacks use the collider as a trigger, and made it so that only the player picking it up destroys it, no more zombies eating healthpacks, or getting stuck on them instead. -Also added health pack spawn to the middle of emptyRooms, yay survivabilitys

--- Assets/Scripts/HealOnCollide.cs
+++ Assets/Scripts/HealOnCollide.cs
@@ -11,27 +11,22 @@ public class HealOnCollide : MonoBehaviour {
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-
-	void OnCollisionEnter2D(Collision2D collision)
-	{
-		OnCollisionStay2D(collision);
-	}
-
-	void OnCollisionStay2D(Collision2D collision)
+	//if player touches, heal them and destory, else nothing.
+	void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.gameObject.tag == "Player")
 		{
 			player = GameObject.FindGameObjectWithTag("Player");
 			playerHealth = player.GetComponent<PlayerHealth>();
 			playerHealth.HealDamage(healAmmount);
+			Destroy(this.gameObject);
 		}
-		Destroy(this.gameObject);
-
 	}
+
+	/**
+	void OnCollisionStay2D(Collision2D collision)
+	{
+	}
+	**/
 }
